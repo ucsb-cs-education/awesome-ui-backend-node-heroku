e5e6fc5c31c41c18ba4e5b868db455fc70ad24a5
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-02 16:51:52 -0700

    Passport google strategy added, inserts user into databsae if new. Ignoring config/auth.js because localhost needs different info.

--- config/passport.js
+++ config/passport.js
@@ -22,4 +22,46 @@ module.exports = function(passport, models) {
         });
     });
 
+
+    // GOOGLE ============================================================
+    passport.use(new GoogleStrategy({
+        clientID : configAuth.googleAuth.clientID,
+        clientSecret : configAuth.googleAuth.clientSecret,
+        callbackURL : configAuth.googleAuth.callbackURL
+    },
+    function(token, refreshToken, profile, done) {
+
+        // make the code asynchronous
+        // Database query will wait until we have all our data back from Google
+        process.nextTick(function() {
+            // try to find the user based on their google id
+            models.User.findOne({
+              where: { id: profile.id, account_type: "google" }
+            }).then(function(user) {
+              if (user) {
+                return done(null, user);
+              } else {
+                models.User.create({
+                    account_type : "google",
+                    id: profile.id,
+                    token: token,
+                    email: profile.emails[0].value,
+                    name: profile.displayName
+                }).then(function(createdUser) {
+                    return done(null, createdUser);
+                }); // don't catch error
+              }
+            }).catch(function(error) {
+                return done(error);
+            });
+            
+        });
+
+    }));
+
+
 }
+
+
+
+

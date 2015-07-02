352d1178b05e9b3daf4d763d36219531d18479e6
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-02 16:24:16 -0700

    Created google Client ID. Google auth information in this file.

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'googleAuth' : {
+		'clientID'		: '881997755531-39cjk7qis9oginlj37f8d61u78cddvc2.apps.googleusercontent.com',
+		'clientSecret'	: 'rcXd9qVD5I1UFwtaUqI5GLCc',
+		'callbackURL'	: 'https://agile-thicket-8103.herokuapp.com/auth/google/callback'
+	}
+}

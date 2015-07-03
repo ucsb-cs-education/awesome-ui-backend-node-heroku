a24ca03af8691d1d9078b2e050bf1dbb8f700a46
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-02 17:35:11 -0700

    Handled get requests to /auth/google and /auth/google/callback for authentication.

--- routes.js
+++ routes.js
@@ -1,24 +1,37 @@
 module.exports = function(app, passport, models) {
 
 	app.get('/', function(request, response) {
-	  response.render('pages/index')
+		response.render('pages/index')
 	});
 
-
 	app.get('/student', function(request, response) {
-	  response.render('pages/student')
+		response.render('pages/student')
 	});
 
 	app.get('/instructor', function(request, response) {
-	  response.render('pages/instructor')
+		response.render('pages/instructor')
 	});
 
 	app.get('/author', function(request, response) {
-	  response.render('pages/author')
+		response.render('pages/author')
 	});
 
 	app.get('/developer', function(request, response) {
-	  response.render('pages/developer')
+		response.render('pages/developer')
 	});
+
+
+
+
+
+	app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+    	passport.authenticate('google', {
+    		successRedirect : '/',
+    		failureRedirect : '/fail'
+    	}));
+
 
 }

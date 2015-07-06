36271f01da59f72e124f917a9710e150e3b336bd
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-06 16:31:12 -0700

    Started routing tests. Not complete.

---
+++ test/routes.js
@@ -0,0 +1,67 @@
+var app = require('../index.js')
+var expect = require("chai").expect;
+var request = require('supertest');
+
+
+describe('GET /', function(){
+  it('respond with plain text', function(){
+    request(app)
+    .get('/')
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      done();
+    });
+  });
+});
+
+
+// Mocha cheatsheet
+/*
+describe('test suite', function () {
+  beforeEach(function() { 
+  	// ...
+  });
+  afterEach(function() { 
+  	// ...
+  });
+
+  before(function() { 
+  	// ...
+  });
+  after(function() { 
+  	// ...
+  });
+
+  it('a basic test', function() { 
+  	// ...
+  });
+
+  it('a test with a promise', function() {
+    return somePromiseObject; });
+
+  it('an asynchronous test', function(next) {
+    if (success) { next(); } else { next(error); }
+  });
+
+  xit('use "xit" for pending tests', function() { 
+  	// ...
+  });
+});
+*/
+
+// Chai cheatsheet
+/*
+expect(3).to.eql(2);
+
+expect(obj).to.be.a('string');
+expect(obj).to.be.null;
+expect(obj).to.be.true;
+expect(obj).to.be.false;
+expect(obj).to.be.undefined;
+
+expect(list).to.include("item");
+expect(list).to.have.length(3);
+expect(list).to.have.length.gt(0);
+*/
+
+

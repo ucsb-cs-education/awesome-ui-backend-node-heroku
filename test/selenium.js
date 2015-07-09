953a42719bb1058ac43733e358d3eb13316f7a56
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-08 18:56:06 -0700

    Facebook login selenium test now programmatically creates test account for testsand deletes it afterwards.

--- test/selenium.js
+++ test/selenium.js
@@ -2,16 +2,18 @@ var wd = require('wd');
 var config = require('../config/sauce.json');
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
-
-
+var querystring = require('querystring');
+var request = require('request');
+var authConfig;
 
 var os = require('os');
-var seleniumURL;
 var isLocal = (os.hostname().indexOf("local") > -1);
 if(isLocal) {
-    URL = 'http://localhost:5000';
+    authConfig = require('../config/authentication.json')['localhost'];
+    baseURL = 'http://localhost:5000';
 } else {
-	URL = 'https://agile-thicket-8103.herokuapp.com';
+    authConfig = require('../config/authentication.json')['development'];
+	baseURL = 'https://agile-thicket-8103.herokuapp.com';
 }
 
 
@@ -42,11 +44,13 @@ desired.tags = ['tutorial'];
 
 describe('Selenium Logging in and out (' + desired.browserName + ')', function() {
 	if (isLocal) {
-		this.timeout(100000);
+		this.timeout(60000);
 	} else {
-		this.timeout(10000);
+		this.timeout(60000);
 	}
     var browser;
+    var accessToken; // used for facebook api request
+    var testUser;
     var allPassed = true;
 
     before(function(done) {
@@ -55,9 +59,29 @@ describe('Selenium Logging in and out (' + desired.browserName + ')', function()
     	} else {
 	        browser = wd.promiseChainRemote("ondemand.saucelabs.com", 80, config.SAUCE_USERNAME, config.SAUCE_ACCESS_KEY);
     	}
-        browser
-            .init(desired)
-            .nodeify(done);
+
+        // get facebook access token
+        request.get("https://graph.facebook.com/oauth/access_token?client_id=" + authConfig.facebookAuth.clientID + 
+            "&client_secret=" + authConfig.facebookAuth.clientSecret + "&grant_type=client_credentials",
+            function (error, response, body) {
+                console.log('Got facebook access token.')
+                accessToken = querystring.parse(body).access_token;
+                request.post(
+                    'https://graph.facebook.com/v2.4/' + authConfig.facebookAuth.clientID + '/accounts/test-users',
+                    { json: { access_token: accessToken } },
+                    function (error, response, body) {
+                        testUser = body;
+                        console.log("Created test user with email " + testUser.email);
+
+                        console.log('now were done.');
+                        browser
+                            .init(desired)
+                            .nodeify(done);
+                    }
+                );
+            }
+        );
+
     });
 
     afterEach(function(done) {
@@ -66,29 +90,41 @@ describe('Selenium Logging in and out (' + desired.browserName + ')', function()
     });
 
     after(function(done) {
-    	if (isLocal) {
-	        browser
-	            .quit()
-	            .nodeify(done);
-    	} else {
-	        browser
-	            .quit()
-	            .sauceJobStatus(allPassed)
-	            .nodeify(done);
-    	}
+
+        // Delete test user we created.
+        request.del(
+            'https://graph.facebook.com/v2.4/' + testUser.id,
+            { json: { access_token: accessToken } },
+            function (error, response, body) {
+                console.log("Deleted user with email: " + testUser.email);
+
+                if (isLocal) {
+                    browser
+                        .quit()
+                        .nodeify(done);
+                } else {
+                    browser
+                        .quit()
+                        .sauceJobStatus(allPassed)
+                        .nodeify(done);
+                }
+            }
+        );
+
     });
 
-    it("should log in then out", function(done) {
+
+    it("should log in with Facebook then out", function(done) {
         browser
-            .get(URL)
+            .get(baseURL)
             .elementById('facebook-login')
             .click()
             .eval("window.location.href")
             .should.eventually.include("facebook.com")
             .elementById("email")
-            .type("khdrsbt_liangman_1436318828@tfbnw.net")
+            .type(testUser.email)
             .elementById("pass")
-            .type("projectawesometestpassword")
+            .type(testUser.password)
             .elementById("u_0_2")
             .click()
             .eval("window.location.href", function(err, href) {
@@ -108,22 +144,27 @@ describe('Selenium Logging in and out (' + desired.browserName + ')', function()
             .waitForElementById("logout", wd.asserters.isDisplayed, 3000)
             .click()
             .eval("window.location.href")
-            .should.eventually.equal(URL + '/')
+            .should.eventually.equal(baseURL + '/')
             .elementById('facebook-login')
             .nodeify(done)
             
-            
-            /*
-            .should.eventually.include(URL)
-            .elementById("logout")
+    });
+
+    it("should log in with Facebook then out", function(done) {
+        browser
+            .get(baseURL)
+            .elementById('facebook-login')
             .click()
             .eval("window.location.href")
-            .should.eventually.equal(URL + '/')
+            .waitForElementById("logout", wd.asserters.isDisplayed, 3000)
+            .click()
+            .eval("window.location.href")
+            .should.eventually.equal(baseURL + '/')
             .elementById('facebook-login')
-            .nodeify(done);
-            */
-            // #platformDialogForm
+            .nodeify(done)
+            
     });
+
 });
 
 

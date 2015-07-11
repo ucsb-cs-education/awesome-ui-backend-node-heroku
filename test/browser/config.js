7c897195db4fe0f425b4cb5319db480e4aaaab30
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-10 18:41:33 -0700

    Removed sauce labs password / username.

--- test/browser/config.js
+++ test/browser/config.js
@@ -1,6 +1,6 @@
 exports.config ={
 	specs:['index.js'],
-	baseUrl: 'http://' + (process.env.CI ? 'localhost' : 'localhost') + ':5000',
+	baseUrl: 'http://localhost:5000',
 	framework:'mocha',
 	mochaOpts: {
 		reporter:'spec',
@@ -17,10 +17,10 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
     exports.config.capabilities = {
         'name': 'Travis + Protractor Tests',
         'browserName': 'chrome',
-        'username': 'wolfenbarger',
-        'accessKey': '31a62fcb-50f1-41b6-b89c-a53e7d6be6e8',
+        'username': process.env.SAUCE_USERNAME,
+        'accessKey': process.env.SAUCE_ACCESS_KEY,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
-        'tags': [process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER, process.env.SAUCE_USERNAME, 'e2e']
+        'tags': [process.env.TRAVIS_BRANCH, process.env.TRAVIS_BUILD_NUMBER, 'e2e']
     };
 }

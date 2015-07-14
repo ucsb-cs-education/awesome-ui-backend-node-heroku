2fa71d5da9b4baa3d22f2a0e990fb5d04a19ae20
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-14 11:27:10 -0700

    Made navigation bar using angular. Added a controller + filter.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("awesomeApp", []);
+var awesomeApp = angular.module('awesomeApp', []);
+
+
+// Navigation Bar
+awesomeApp.controller('NavBarCtrl', function($scope) {
+	$scope.pages = ['student', 'instructor', 'author', 'developer'];
+});
+
+
+
+awesomeApp.filter('capitalizeFirst', function() {
+    return function(input, scope) {
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+});

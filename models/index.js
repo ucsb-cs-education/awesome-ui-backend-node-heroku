d659106762caf7991c3c1d0eb7acc0adfa56ac0b
Phill Conrad <pconrad@cs.ucsb.edu>
2015-07-17 14:59:58 -0700

    Fixed stub for QuizDescriptor and setup of env for testing

--- models/index.js
+++ models/index.js
@@ -6,13 +6,16 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.DATABASE_URL, { });
 
+var doNotSequelizeThese = ["index.js","QuizDescriptor.js"];
+
 // Loop through all files in /models and Sequelize import them
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
+	  return (file.indexOf(".") !== 0) && (doNotSequelizeThese.indexOf(file)==-1)
   })
   .forEach(function(file) {
+    console.log("file=" + file);
     var model = sequelize["import"](path.join(__dirname, file));
     module.exports[model.name] = model;
   });

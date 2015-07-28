7d04beb528a3f4d397c2e5a0f0a295c4d4245b4f
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-27 20:10:29 -0700

    quizdescriptor/:id page now exists and passes route tests.

--- routes/page_routes.js
+++ routes/page_routes.js
@@ -1,34 +1,4 @@
-module.exports = function(app) {
-
-	app.get('/', function(request, response) {
-		response.render('pages/index', { user : request.user })
-	});
-
-	app.get('/student', function(request, response) {
-		response.render('pages/student', { user : request.user })
-	});
-
-	app.get('/instructor', function(request, response) {
-		response.render('pages/instructor', { user : request.user } )
-	});
-
-	app.get('/author', function(request, response) {
-		response.render('pages/author', { user : request.user })
-	});
-
-	app.get('/developer', function(request, response) {
-		response.render('pages/developer', { user : request.user })
-	});
-
-	app.get('/usersettings', redirectUnauthenticatedToLogin, function(request, response) {
-		response.render('pages/usersettings', { user : request.user })
-	});
-
-	app.get('/login', redirectAuthenticatedToPreferred, function(request, response) {
-		response.render('pages/login', { user : request.user })
-	});
-}
-
+var models = require('../models');
 
 function redirectUnauthenticatedToLogin(req, res, next) {
     if (req.isAuthenticated())
@@ -41,6 +11,54 @@ function redirectAuthenticatedToPreferred(req, res, next) {
         return next();
     res.redirect('/' + req.user.role);
 }
+
+module.exports = function(app) {
+
+	app.get('/', function(req, res) {
+		res.render('pages/index', { user : req.user });
+	});
+
+	app.get('/student', function(req, res) {
+		res.render('pages/student', { user : req.user });
+	});
+
+	app.get('/instructor', function(req, res) {
+		res.render('pages/instructor', { user : req.user } );
+	});
+
+	app.get('/author', function(req, res) {
+		res.render('pages/author', { user : req.user });
+	});
+
+	app.get('/developer', function(req, res) {
+		res.render('pages/developer', { user : req.user });
+	});
+
+	app.get('/usersettings', redirectUnauthenticatedToLogin, function(req, res) {
+		res.render('pages/usersettings', { user : req.user });
+	});
+
+	app.get('/quizdescriptor/:id', function(req, res) {
+		models.QuizDescriptor.findOne({ where: {id: req.params.id} })
+		.then(function(qd) {
+			if (qd) {
+				qd.descriptor = JSON.parse(qd.descriptor);
+				res.render('pages/quizdescriptor', { user : req.user, qd : qd });
+			} else {
+				res.status(404).send('Sorry cant find that!');
+			}
+		}, function(error) {
+			res.status(404).send('Sorry cant find that!');
+		});
+		
+	});
+
+	app.get('/login', redirectAuthenticatedToPreferred, function(req, res) {
+		res.render('pages/login', { user : req.user });
+	});
+}
+
+
 
 
 

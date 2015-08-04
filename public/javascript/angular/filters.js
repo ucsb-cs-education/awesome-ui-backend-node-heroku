b77f5f9307030ef22bc4c3a1c6e708ae322ace93
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-04 14:48:56 -0700

    Added filter + tests to convert indices to corresponding alphabet letter.

---
+++ public/javascript/angular/filters.js
@@ -0,0 +1,8 @@
+'use strict';
+
+awesomeApp.filter('indexToLetter', function() {
+	return function(index) {
+		return (index >= 0 && index <= 25) ? String.fromCharCode(97 + index) : index;
+	};
+});
+

8c945133aa56346a8c75d4ffd1264bb57c526b50
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-04 18:55:08 -0700

    WIP

--- test/angular-unit/filters.js
+++ test/angular-unit/filters.js
@@ -1,3 +1,24 @@
 
 
 
+describe('Angular Filters', function() {
+
+	beforeEach(module('awesomeApp'));
+
+	describe('indexToLetter', function() {
+
+		it('should convert indices 0-25 to a-z', function() {
+
+		});
+
+
+	});
+});
+
+
+
+
+
+
+
+

bd5a416a57bbafecc7476d8f17928a6180f62d63
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-04 18:50:35 -0700

    Still not working.

--- test/angular-unit/filters.js
+++ test/angular-unit/filters.js
@@ -1,32 +1,3 @@
 
 
 
-describe('Angular Filters', function() {
-
-	beforeEach(module('awesomeApp'));
-
-	describe('indexToLetter', function() {
-
-		it('should convert indices 0-25 to a-z',
-			inject(function(indexToLetterFilter) {
-			expect(indexToLetterFilter(0)).to.equal('a');
-			expect(indexToLetterFilter(25)).to.equal('z');
-		}));
-
-		it('should leave any other index value alone',
-			inject(function(indexToLetterFilter) {
-			expect(indexToLetterFilter(-1)).to.equal(-1);
-			expect(indexToLetterFilter(26)).to.equal(26);
-		}));
-
-
-	});
-});
-
-
-
-
-
-
-
-

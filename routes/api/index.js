a8b9609ea106e7668e23fb705fadfe7c6dbe6cd6
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-08 13:17:19 -0700

    API + test for moodleXML questions exporting.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
 	require('./user')(app);
 	require('./qd')(app);
 	require('./quiz')(app);
+	require('./question')(app);
 	app.get('/api/*', function(req, res) {
         res.status(404).end();
     });
---
+++ routes/api/question.js
@@ -0,0 +1,36 @@
+var projectAwesome = require("project-awesome");
+
+function isValidId(n) {
+    return /^\d+$/.test(n);
+}
+
+module.exports = function(app) {
+
+    app.get('/api/question/moodle/:questionType', function(req, res) {
+        if (!projectAwesome.isValidQuestionType(req.params.questionType)) {
+            res.status(404).end();
+            return;
+        }
+        var count = 20;
+        if (req.query.count) {
+            count = parseInt(req.query.count);
+            if (isNaN(count) || count < 1 || count > 999) {
+                res.status(400).end();
+                return;
+            }
+        }
+        res.set('Content-Type', 'text/xml');
+        try {
+            res.send(projectAwesome.generateMoodleXML(req.params.questionType, count, req.params.questionType));
+        } catch (e) {
+            res.status(400).end();
+        }
+        
+        
+    });
+
+}
+
+
+
+

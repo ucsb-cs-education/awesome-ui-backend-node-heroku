c32f41762d020c992c48eccd5f9828b89407c9b8
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-17 20:44:31 -0700

    Tests for adding quiz descriptors in /instructor/quizdescriptors

---
+++ test/e2e/pages/instructor.quizdescriptors.test.js
@@ -0,0 +1,92 @@
+var chai = require('chai');
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var app = require('../../../app.js');
+var models = require('../../../models');
+var utils = require('../../utils');
+var server;
+
+
+describe('/instructor/quizdescriptors', function() {
+	var qd, testUser;
+    before(function(done) {
+        models.sequelize.sync({ force: true }).then(function () {
+            server = app.listen(app.get('port'), function() {
+	            models.sequelize.sync({ force: true }).then(function () {
+	                utils.insertQuizDescriptor(models, 'Example Quiz Descriptor Title').then(function(res) {
+	                    qd = res;
+                        utils.protractorLogin().then(function(user) {
+                            testUser = user;
+                            browser.get('/instructor/quizdescriptors').then(function() {
+                                done();
+                            });
+                        });
+	                });
+	            });
+            });
+        });
+    });
+
+    after(function(done) {
+        server.close();
+        done();
+    });
+
+    describe('quiz descriptor list', function() {
+        it('should contain all quiz descriptors', function(done) {
+            expect(element.all(by.repeater('quiz in quizDescriptors.quizzes')).count()).to.eventually.equal(1);
+            expect(element(by.repeater('quiz in quizDescriptors.quizzes').row(0).column('quiz.id')).getText()).to.eventually.equal(qd.id+'');
+            expect(element(by.repeater('quiz in quizDescriptors.quizzes').row(0).column('quiz.descriptor')).getText()).to.eventually.include(JSON.stringify(qd.descriptor).substring(0,80));
+            done();
+        });
+    });
+
+    describe('Add New', function() {
+        var addNew, textArea;
+        before(function(done) {
+            addNew = element(by.buttonText('Add New'));
+            textArea = element(by.id('comment'));
+            done();
+        });
+
+        beforeEach(function(done) {
+            textArea.clear();
+            done();
+        });
+
+        describe('invalid quiz descriptor', function() {
+            it('should not add the qd to the list', function(done) {
+                textArea.sendKeys('{"ayyyy":"lmao"}}');
+                addNew.click();
+                expect(element(by.id('flash-message-div')).getText()).to.eventually.include('Not Saved: Invalid Syntax.');
+                expect(element.all(by.repeater('quiz in quizDescriptors.quizzes')).count()).to.eventually.equal(1);
+                done();
+            });
+        });
+
+
+        describe('valid quiz descriptor', function() {
+            it('should add the qd to the list and display a success flash message', function(done) {
+                var sampleQD = utils.getSampleQuizDescriptor('testQD1');
+                textArea.sendKeys(JSON.stringify(sampleQD));
+                addNew.click();
+                expect(element(by.id('flash-message-div')).getText()).to.eventually.include('Quiz Descriptor Saved');
+                expect(element.all(by.repeater('quiz in quizDescriptors.quizzes')).count()).to.eventually.equal(2);
+                done();
+            });
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+

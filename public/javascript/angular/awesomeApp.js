9619ff3bb68c2698e3a5c349b4bdad4835611a66
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-18 17:00:05 -0700

    Added google analytics, fixed for angularjs SPA. Removed pageview in ga script. Added it on stateChangeSuccess.

--- public/javascript/angular/awesomeApp.js
+++ public/javascript/angular/awesomeApp.js
@@ -111,7 +111,7 @@ awesomeApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
 		return $location.path();
 	});
 }])
-.run(['AuthService', 'Flash', '$rootScope', '$state', function(AuthService, Flash, $rootScope, $state) {
+.run(['AuthService', 'Flash', '$rootScope', '$state', '$window', function(AuthService, Flash, $rootScope, $state, $window) {
 
 	$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
 		if (error.status == 404) {
@@ -143,6 +143,10 @@ awesomeApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
  
 		}
 
+	});
+
+	$rootScope.$on("$stateChangeSuccess", function(event) {
+		$window.ga('send', 'pageview', { page: $state.current.url });
 	});
 }]);
 
